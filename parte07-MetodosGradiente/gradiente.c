9b95e95ca3180dc677665392e83f1ab90b1dcd08
cycifus <victorbarboza7657@gmail.com>
2020-11-13 13:11:19 -0300

    13-11-2020

--- parte07-MetodosGradiente/gradiente.c
+++ parte07-MetodosGradiente/gradiente.c
@@ -69,37 +69,27 @@ double norma(double *r, int m){
 
 double *gradiente(double **A, int m, double *x, double *r){
     int i;
-    double lambda, denominador, numerador,*auxVector, *prodRoA;
+    double lambda, denominador, numerador;
 
-    auxVector = (double *)malloc(m*sizeof(double));
-
-    numerador = fabs(dotProduct(r,m,r));
-    prodRoA = prodMatrixVetor(A,m,r); // return vector
-    denominador = fabs(dotProduct(prodRoA,m,r)); 
+    numerador = dotProduct(r,m,r);
+    denominador = dotProduct(prodMatrixVetor(A,m,r),m,r); 
     lambda = numerador/denominador;
 
     for(i=0; i<m; i++){
-        auxVector[i] = x[i] + lambda*r[i];
+        x[i] = x[i] + lambda*r[i];
     }
 
-    return auxVector;
+    return x;
 }
 
 int main(int argc, char **argv)
 {
-    //FILE *fp;
-    
     int m,n,i,iterations=0;
     double **M, *x0, *r0, tolerance=1e-8;
-
-    //fp = fopen("saida.csv","w");
-
-    //fprintf(fp,"it,tolerance\n");
 
     m = atoi(argv[1]);
     n = atoi(argv[2]);
     M = LeMatrix(m,n);
-    //ImprimeMatriz(M,m,n);   
   
     x0 = (double *) malloc(m*sizeof(double));
     for(i=0;i<m;i++) scanf("%lf", &x0[i]);
